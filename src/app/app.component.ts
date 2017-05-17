408d270c95489491c428319605b9e7ad868b7c53
eamanu <emmanuelarias30@gmail.com>
2017-05-16 21:45:09 -0300

    Commit before to merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'Rescate Animal';
+  hero : Hero = {
+    id: 1,
+    name: "Windstorm"
+  }
+}
+export class Hero{
+  id: number;
+  name: string;
 }
